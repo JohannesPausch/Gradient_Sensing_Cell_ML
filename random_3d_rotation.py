d875ff66bd5ba5021574a3b0facdc983ff59bc62
Johannes Pausch <JohannesPausch@users.noreply.github.com>
2021-07-07 13:03:34 +0100

    random_3rd_rotations
    random_3rd_rotation.py provides a module which takes numpy arrays of (phi,theta) pairs and applies a random rotation to them. They all are subject to the _same_ random rotation. The seed can be fixed if needed.
    
    visualize_3d_angles.py is script that tests the module random_3rd_rotation and visualizes its output.

---
+++ random_3d_rotation.py
@@ -0,0 +1,19 @@
+ # random_3d_rotation : randomly change orientation of 3-dimensional data
+from scipy.stats import special_ortho_group
+import numpy as np
+
+# takes azimuth phi (0,pi) and polar angle theta (0,2pi), turns them randomly and returns their turned version. 
+# numpy array of phi and theta can be used but must have the same shape
+# The used distribution on the space of rotations is the Haar measure, i.e. it is uniform on SO(3)
+# if the seed can be specified using use_seed. If not specfied, seeds will change automatically
+def random_3d_rotation(phi,theta,use_seed=None):   
+    try:
+        np.random.seed(seed=use_seed)
+        y=np.matmul(special_ortho_group.rvs(3),np.array([np.cos(theta)*np.sin(phi),np.sin(theta)*np.sin(phi),np.cos(phi)]))#special_ortho_group.rvs(3)
+        return np.arccos(y[2]), np.mod(np.arctan2(y[1],y[0]),2*np.pi)
+    except ValueError:
+        if phi.shape != theta.shape:  print("Error in random_3d_rotation: Shapes of phi and theta don't match")
+        else: print("Unknown error in random_3d_rotation")
+        return phi,theta
+
+
---
+++ visualize_3d_angles.py
@@ -0,0 +1,54 @@
+# visualization of random_3d_rotation
+
+import numpy as np
+import random_3d_rotation
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import gridspec
+
+data_number = 10000
+original_phi = np.zeros(data_number)
+original_theta = np.zeros(data_number)
+new_phi = np.zeros(data_number)
+new_theta = np.zeros(data_number)
+new_x = np.zeros(data_number)
+new_y = np.zeros(data_number)
+new_z = np.zeros(data_number)
+
+for i in range(data_number):
+    new_phi[i],new_theta[i] = random_3d_rotation.random_3d_rotation(original_phi[i],original_theta[i])
+    new_x[i] = np.cos(new_theta[i])*np.sin(new_phi[i])
+    new_y[i] = np.sin(new_theta[i])*np.sin(new_phi[i])
+    new_z[i] = np.cos(new_phi[i])
+
+surface_phi, surface_theta = np.mgrid[0.0:np.pi:100j, 0.0:2.0*np.pi:100j]
+surface_radius = 0.95
+surface_x = surface_radius*np.cos(surface_theta)*np.sin(surface_phi)
+surface_y = surface_radius*np.sin(surface_theta)*np.sin(surface_phi)
+surface_z = surface_radius*np.cos(surface_phi)
+
+bins = 60
+sample_every = 20
+fig = plt.figure(figsize=[6.4,6])
+spec = gridspec.GridSpec(ncols=2, nrows=2, hspace=0.4, height_ratios=[1,5])
+ax1 = fig.add_subplot(spec[0])
+ax1.set_xlim([0,np.pi])
+ax1.set_xlabel(r'$\phi$')
+ax1.set_ylabel(r'occurrence')
+ax1.set_xticks([0,np.pi/2, np.pi])
+ax1.set_xticklabels([r'$0$', r'$\pi/2$', r'$\pi$'])
+ax1.hist(new_phi,bins)
+ax2 = fig.add_subplot(spec[1])
+ax2.set_xlim([0,2*np.pi])
+ax2.set_xlabel(r'$\theta$')
+ax2.set_xticks([0,np.pi, 2*np.pi])
+ax2.set_xticklabels([r'$0$', r'$\pi$', r'$2\pi$'])
+ax2.hist(new_theta,bins)
+ax3 = fig.add_subplot(spec[2:], projection='3d')
+ax3.set_xticks([-1,0,1])
+ax3.set_yticks([-1,0,1])
+ax3.set_zticks([-1,0,1])
+ax3.plot_surface(surface_x,surface_y,surface_z,  rstride=1, cstride=1, color='c', alpha=1.0, linewidth=0)
+ax3.scatter(new_x[1::sample_every],new_y[1::sample_every],new_z[1::sample_every],color="k",s=20)
+plt.savefig("RandomPhiTheta01.png")
