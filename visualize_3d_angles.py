095bd55bea93bee267a299cf34faa10fad8ed28f
Johannes Pausch <JohannesPausch@users.noreply.github.com>
2021-07-07 18:36:43 +0100

    new version of visualize_3d_angles
    the new version shows that orientation of a line is change too

--- visualize_3d_angles.py
+++ visualize_3d_angles.py
@@ -22,11 +22,22 @@ for i in range(data_number):
     new_y[i] = np.sin(new_theta[i])*np.sin(new_phi[i])
     new_z[i] = np.cos(new_phi[i])
 
-surface_phi, surface_theta = np.mgrid[0.0:np.pi:100j, 0.0:2.0*np.pi:100j]
-surface_radius = 0.95
-surface_x = surface_radius*np.cos(surface_theta)*np.sin(surface_phi)
-surface_y = surface_radius*np.sin(surface_theta)*np.sin(surface_phi)
-surface_z = surface_radius*np.cos(surface_phi)
+phi_line=np.arange(0,np.pi/2,0.1)
+theta_line=5.7*np.ones(len(phi_line))
+new_phi_line,new_theta_line = random_3d_rotation.random_3d_rotation(phi_line,theta_line)
+x_line = np.cos(theta_line)*np.sin(phi_line)
+y_line = np.sin(theta_line)*np.sin(phi_line)
+z_line = np.cos(phi_line)
+new_x_line = np.cos(new_theta_line)*np.sin(new_phi_line)
+new_y_line = np.sin(new_theta_line)*np.sin(new_phi_line)
+new_z_line = np.cos(new_phi_line)
+
+
+#surface_phi, surface_theta = np.mgrid[0.0:np.pi:100j, 0.0:2.0*np.pi:100j]
+#surface_radius = 0.9
+#surface_x = surface_radius*np.cos(surface_theta)*np.sin(surface_phi)
+#surface_y = surface_radius*np.sin(surface_theta)*np.sin(surface_phi)
+#surface_z = surface_radius*np.cos(surface_phi)
 
 bins = 60
 sample_every = 20
@@ -47,8 +58,13 @@ ax2.set_xticklabels([r'$0$', r'$\pi$', r'$2\pi$'])
 ax2.hist(new_theta,bins)
 ax3 = fig.add_subplot(spec[2:], projection='3d')
 ax3.set_xticks([-1,0,1])
+ax3.set_xlabel(r'$x$')
 ax3.set_yticks([-1,0,1])
+ax3.set_ylabel(r'$y$')
 ax3.set_zticks([-1,0,1])
-ax3.plot_surface(surface_x,surface_y,surface_z,  rstride=1, cstride=1, color='c', alpha=1.0, linewidth=0)
+ax3.set_zlabel(r'$z$')
+#ax3.plot_surface(surface_x,surface_y,surface_z,  rstride=1, cstride=1, color='c', alpha=1.0, linewidth=0)
 ax3.scatter(new_x[1::sample_every],new_y[1::sample_every],new_z[1::sample_every],color="k",s=20)
+ax3.plot3D(x_line,y_line,z_line,'blue')
+ax3.plot3D(new_x_line,new_y_line,new_z_line,'red')
 plt.savefig("RandomPhiTheta01.png")
