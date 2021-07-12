1f78ace7842d10a2852e9bb8d289cd58e69972ed
paulagrcg <31971325+paulagrcg@users.noreply.github.com>
2021-07-12 15:32:12 +0200

    Update ML_linear_regression.py

--- ML_linear_regression.py
+++ ML_linear_regression.py
@@ -16,93 +16,68 @@ event=data[:,0]
 iteration=data[:,1]
 theta=data[:,2]
 phi=data[:,3]
-x=data[:,4]
-y=data[:,5]
-z=data[:,6]
+z=data[:,4] #x before
+y=data[:,5] #y
+x=data[:,6] #z
 t=data[:,7]
 numtrain=len(x)+1
 
 
-ytheta=source_theta_rel_sphere=np.zeros(numtrain-1) # theta of source
-yphi=source_phi_rel_sphere=np.zeros(numtrain-1)# phi of source, doesn't matter when source in the x axis
-
+Y=np.zeros((1,2)) #origin source
+X=np.transpose(np.concatenate((np.transpose(theta).reshape(1,numtrain-1),np.transpose(phi).reshape(1,numtrain-1))))
 ####################FUNCTIONS###################################
   
 def analyticalreg(x,y): 
     #method to solve for beta analytically
     #(X^tX)^-1X^ty
-    x=np.transpose(x)
+    
     return np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(x),x)),np.transpose(x)),y)
 
-def random_3d_rotation(phi,theta,use_seed=None):  #modified  
+def random_3d_rotation(theta,phi,use_seed=None):   
     try:
         np.random.seed(seed=use_seed)
-        y=np.matmul(special_ortho_group.rvs(3),np.array([np.cos(theta),np.sin(theta)*np.cos(phi),np.sin(theta)*np.sin(phi)]))#special_ortho_group.rvs(3)
-        return np.arccos(y[0]), np.mod(np.arctan2(y[2],y[1]),2*np.pi)
+        y=np.matmul(special_ortho_group.rvs(3),np.array([np.cos(phi)*np.sin(theta),np.sin(phi)*np.sin(theta),np.cos(theta)]))#special_ortho_group.rvs(3)
+        return np.arccos(y[2]), np.mod(np.arctan2(y[1],y[0]),2*np.pi)
     except ValueError:
         if phi.shape != theta.shape:  print("Error in random_3d_rotation: Shapes of phi and theta don't match")
         else: print("Unknown error in random_3d_rotation")
-        return phi,theta
-    
-def addsources(theta,phi,ytheta,yphi,nsources):
-    lentrain=len(theta)
-    origintheta=theta
-    originphi=phi
-    for i in range(1,nsources):
-        
-        [rytheta,ryphi]=random_3d_rotation(0,0,i)
-        rytheta=rytheta*np.ones(lentrain)
-        ryphi=ryphi*np.ones(lentrain)
-
-        [rtheta,rphi]=random_3d_rotation(origintheta,originphi,i)
-   
-        ytheta=np.concatenate((ytheta,rytheta))
-        yphi=np.concatenate((yphi,ryphi))
-        theta=np.concatenate((theta,rtheta))
-        phi=np.concatenate((phi,rphi))
-    return [theta,phi,ytheta,yphi]
-
-def factors(x):
-   f=[0]
-   for i in range(1, x + 1):
-       if x % i == 0 and i % 3 ==0 :
-          f.append(i)
-   return f
-
-def closest(lst, K):
-    return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
-    
-#https://www.geeksforgeeks.org/python-find-closest-number-to-k-in-given-list/
+        return theta,phi
 
 
-nsources=6
-[theta,phi,ytheta,yphi]=addsources(theta,phi,ytheta,yphi,nsources)
+def reshapevalues(theta,phi):
+    X = np.empty((2*theta.size), dtype=theta.dtype) 
+    X[0::2]=theta
+    X[1::2]=phi
+    return X
 
 ##################### SET UP VARIABLES  ######################
-groupsizeapprox=30
-totdat=len(theta)
-groupsize=closest(factors(totdat),groupsizeapprox)
+groupsize=40
+
+#sweep :
+
+for i in range(0,numtrain-1):
+    if (i+groupsize==numtrain-1):
+        break
+    else:
+        theta=X[i:i+groupsize,0]
+        phi=X[i:i+groupsize,1]
+        Yr=np.array(random_3d_rotation(Y[0,0],Y[0,1])).reshape(1,2)
+        [rtheta,rphi]=random_3d_rotation(theta,phi)
+        Xr=reshapevalues(rtheta,rphi).reshape(1,2*groupsize)
+        if i==0:
+            Xi=reshapevalues(theta,phi).reshape(1,2*groupsize)
+            Xtot=np.concatenate((Xi,Xr))
+            Ytot= np.concatenate((Y, Yr))
+        else: 
+            Xtot=np.concatenate((Xtot,Xr))
+            Ytot= np.concatenate((Ytot, Yr))
+        
+
+##################### regression ############################################
+
+beta= analyticalreg(Xtot, Ytot)
 
 
-X=np.concatenate((np.ones(nsources*(numtrain-1)).reshape(1,nsources*(numtrain-1)),np.transpose(theta).reshape(1,nsources*(numtrain-1)),np.transpose(phi).reshape(1,nsources*(numtrain-1))))
-                 ## include steps?
-X1= np.zeros(3*totdat).reshape(groupsize,int(3*totdat/groupsize))
-for j in range(0,totdat):
-    for k in range(0,int(3*totdat/groupsize)):
-        for i in np.array(range(0,groupsize))[::3]:
-            X1[i:i+3,k]=X[:,j]
-
-#X1=X.reshape(groupsize,int(3*totdat/groupsize)) doesn't work
-
-Y = (np.concatenate((ytheta,yphi)).reshape(2,nsources*(numtrain-1))).T
-
-Y1= np.zeros(3*totdat).reshape(groupsize,int(3*totdat/groupsize))
-for j in range(0,totdat):
-    for k in range(0,int(3*totdat/groupsize)):
-        for i in np.array(range(0,groupsize))[::2]:
-            Y1[i:i+2,k]=Y[j,:]
-            
-beta = np.random.rand(3,2)  #weights
 #######################RUN ITERATIONS(Gradient descent)###############################################
 """
 numiter = 1000
@@ -114,8 +89,7 @@ for i in range(0,numiter): #repeat until convergence
     beta= GradDescent(alpha,beta,X,y1)
     J[i,0]= CostFunction(beta,X,y1)
 """ 
-##################### regression ############################################
 
-beta= analyticalreg(X, Y)
 ##########################VISUALS#############################################
+
 
