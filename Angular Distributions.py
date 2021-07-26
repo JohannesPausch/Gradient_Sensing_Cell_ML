f41a76287f3771e11b9caa5a5a6a0027298b9d07
Johannes Pausch <JohannesPausch@users.noreply.github.com>
2021-07-26 15:04:20 +0100

    small tests

--- Angular Distributions.py
+++ Angular Distributions.py
@@ -4,32 +4,32 @@ import pandas as pd
 
 
 # Load Data
-
-df1 = pd.read_csv('BrownianParticle_RUNRUN01_1_1000_P01_20210705_173431.txt', header = None, sep=" ")
+path = 'BrownianParticle_RUNRUN01/'
+df1 = pd.read_csv(path+'BrownianParticle_RUNRUN01_1_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df1.head()
 
-df2 = pd.read_csv('BrownianParticle_RUNRUN01_2_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df2 = pd.read_csv(path+'BrownianParticle_RUNRUN01_2_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df2.head()
 
-df3 = pd.read_csv('BrownianParticle_RUNRUN01_3_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df3 = pd.read_csv(path+'BrownianParticle_RUNRUN01_3_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df3.head()
 
-df4 = pd.read_csv('BrownianParticle_RUNRUN01_4_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df4 = pd.read_csv(path+'BrownianParticle_RUNRUN01_4_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df4.head()
 
-df5 = pd.read_csv('BrownianParticle_RUNRUN01_5_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df5 = pd.read_csv(path+'BrownianParticle_RUNRUN01_5_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df5.head()
 
-df6 = pd.read_csv('BrownianParticle_RUNRUN01_6_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df6 = pd.read_csv(path+'BrownianParticle_RUNRUN01_6_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df6.head()
 
-df7 = pd.read_csv('BrownianParticle_RUNRUN01_7_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df7 = pd.read_csv(path+'BrownianParticle_RUNRUN01_7_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df7.head()
 
-df8 = pd.read_csv('BrownianParticle_RUNRUN01_8_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df8 = pd.read_csv(path+'BrownianParticle_RUNRUN01_8_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df8.head()
 
-df9 = pd.read_csv('BrownianParticle_RUNRUN01_9_1000_P01_20210705_173431.txt', header = None, sep=" ")
+df9 = pd.read_csv(path+'BrownianParticle_RUNRUN01_9_1000_P01_20210705_173431.txt', header = None, sep=" ")
 df9.head()
 
 
@@ -53,7 +53,7 @@ for i, ax in enumerate(axes.flatten()):
     ax.set_ylabel(r'$Frequency/sin({\Theta})$')
     ax.legend(fontsize = 'large',  loc='upper right')
     plt.tight_layout()
-plt.show()
+#plt.show()
 plt.legend()
 plt.savefig('Theta Distribution for Gradient Sensing.png')
 
@@ -73,7 +73,7 @@ for i, ax in enumerate(axes.flatten()):
     ax.set_ylabel(r'$Frequency$')
     ax.legend(fontsize = 'large',  loc='upper right')
     plt.tight_layout()
-plt.show()
+#plt.show()
 
 plt.legend()
 plt.savefig('Phi Distribution for Gradient Sensing.png')
