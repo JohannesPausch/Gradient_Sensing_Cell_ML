3dafb9aa7f83f979f565863b572f230043e41bd9
paulagrcg <paulagrcg@gmail.com>
2021-07-27 16:49:06 +0100

    receptor map

--- ReceptorMap.py
+++ ReceptorMap.py
@@ -3,21 +3,33 @@ import math
 from pprint import pprint
 from pointssphere import *
 import matplotlib.pyplot as plt
+from areareceptor import * 
+from sphericaltransf import *
+####################################
+#Distribution of receptors:
+
 #This code was taken from GitHub: https://gist.github.com/dinob0t/9597525
 #Uses reference: https://www.cmu.edu/biolphys/deserno/pdf/sphere_equi.pdf
 
+
 radius = 1
-points = 50
+points = 10
 
-#print("Randomly distributed points")
-#x,y,z = random_on_sphere_points(radius,points)
+print("Randomly distributed points")
+x,y,z = random_on_sphere_points(radius,points)
+receptors= np.concatenate(([x],[y],[z])).T
+#pprint(x)
+#pprint(receptors)
+receptornum= len(x)
+theta,phi= sphericaltransf(x,y,z)
+activation_receptor=np.zeros((receptornum))
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x,y,z) 
 
-#fig = plt.figure(figsize=(4,4))
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(x,y,z) 
+plt.show()
 
-#plt.show()
-
+"""
 print("Evenly distributed points")
 x,y,z = regular_on_sphere_points(radius,points)
 
@@ -26,3 +38,21 @@ ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x,y,z) 
 
 plt.show()
+"""
+##############################################
+#Check whether a recetor is close to the molecule
+#activate closest receptor
+#need data of where the molecule hits
+"""
+MinDistance=0.05
+
+for i in range(0,receptornum):
+    finaldistance = 0
+    distance = haversine(radius,molecule_theta,molecule_phi,theta[i],phi[i])
+    if distance < MinDistance and distance < finaldistance:
+        finaldistance = distance
+        finalindex = i
+
+activation_receptor[finalindex] += 1
+"""
+####################################
