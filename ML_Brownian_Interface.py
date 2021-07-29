8ed4b4940461ea28f104d43cafce4018a95d38de
Johannes Pausch <JohannesPausch@users.noreply.github.com>
2021-07-29 12:04:56 +0100

    updated on ML to c-code communication

--- ML_Brownian_Interface.py
+++ ML_Brownian_Interface.py
@@ -1,23 +1,31 @@
 # communication between python ML and c-code BrownianParticle.c 
 
 from subprocess import Popen, PIPE
+separator = ' '
 
-
-def init_BrownianParticle(xpos,ypos,zpos,rate,seed=None):
-    if seed==None:
-        brownian_pipe = Popen(['./BrownianParticle.o'], shell=True, stdout=PIPE, stdin=PIPE)
-    else:
-        brownian_pipe = Popen(['./BrownianParticle.o -S '+str(seed)], shell=True, stdout=PIPE, stdin=PIPE)
-    brownian_pipe.stdin.write(bytes(str(xpos)+','+str(ypos)+','+str(zpos)+'\n', 'UTF-8'))
+def init_BrownianParticle(xpos,ypos,zpos,rate,seed=None,cutoff=None,events=None,iterations=None,sigma=None):
+    command = './BrownianParticle.o'
+    if seed != None:
+        command += ' -S '+str(seed)
+    if cutoff != None:
+        command += ' -c '+str(cutoff)
+    if events != None:
+        command += ' -E '+str(events)
+    if iterations != None:
+        command += ' -N '+str(iterations)
+    if sigma != None:
+        command += ' -s '+str(sigma)
+    brownian_pipe = Popen([command], shell=True, stdout=PIPE, stdin=PIPE)
+    brownian_pipe.stdin.write(bytes(str(xpos)+separator+str(ypos)+separator+str(zpos)+'\n', 'UTF-8'))
     brownian_pipe.stdin.flush()
-    received = brownian_pipe.stdout.readline().strip().decode('ascii').split()
+    received = brownian_pipe.stdout.readline().strip().decode('ascii').split(separator)
     received = [float(x) for x in received]
     return brownian_pipe,received
 
 def update_BrownianParticle(brownian_pipe,xpos,ypos,zpos):
-    brownian_pipe.stdin.write(bytes(str(xpos)+','+str(ypos)+','+str(zpos)+'\n', 'UTF-8'))
+    brownian_pipe.stdin.write(bytes(str(xpos)+separator+str(ypos)+separator+str(zpos)+'\n', 'UTF-8'))
     brownian_pipe.stdin.flush()
-    received = brownian_pipe.stdout.readline().strip().decode('ascii').split()
+    received = brownian_pipe.stdout.readline().strip().decode('ascii').split(separator)
     received = [float(x) for x in received]
     return received
 
