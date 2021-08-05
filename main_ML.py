11447e29723d771e048e079b44864aafc148a3e2
paulagrcg <paulagrcg@gmail.com>
2021-08-05 11:47:03 +0100

    main

--- main_ML.py
+++ main_ML.py
@@ -2,12 +2,15 @@ from ReceptorNeuralNetwork import *
 from datacreation import *
 import numpy as np
 
+######## data ##################
+  
+X, Y = datacreate()
 
-    X, Y = datacreate()
 
-    training_x, training_y, predict_x, predict_y = separate_train_set(X, Y)
-    mlp = train(training_x, training_y, layers_tuple = (6,4), max_iterations=1000)
+###### train and test ##########
+training_x, training_y, predict_x, predict_y = separate_train_set(X, Y)
+mlp = train(training_x, training_y, layers_tuple = (6,4), max_iterations=1000)
 
-    save_neural_network(mlp)
-    restored_mlp = load_neural_network()
-    test(restored_mlp, predict_x, predict_y)
+save_neural_network(mlp)
+restored_mlp = load_neural_network()
+test(restored_mlp, predict_x, predict_y)
