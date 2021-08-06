899e31305be6a90402e81dc38c0add9482637d8b
paulagrcg <paulagrcg@gmail.com>
2021-08-06 18:01:20 +0100

    ideal direction Y

--- IdealDirection.py
+++ IdealDirection.py
@@ -6,48 +6,37 @@ import random
 import math
 import mpl_toolkits.mplot3d
 import matplotlib.pyplot as plt
-import matlab.engine
 
-
-def random_directions(radius, number):
-    # this function uses Paula's receptor code for directions on a sphere
-    x,y,z = random_on_sphere_points(radius, number-1) 
+def random_directions(number):
+    # Cart coords not necessary just theta and phi
+    x,y,z = random_on_sphere_points(1, number-1) 
     theta,phi= cart2spherical_receptors(x,y,z) 
     direction_sphcoords= np.array([theta,phi]).T
-    direction_cartcoords= np.array([x,y,z]).T
 
-    return direction_sphcoords, direction_cartcoords
+    return direction_sphcoords
     
     
-def regular_directions(radius, number):
-    # this function uses Paula's receptor code for regularly spaced directions on a sphere
-    x,y,z = regular_on_sphere_points(radius, number-1)
+def regular_directions(number):
+    # Cart coords not necessary just theta and phi
+    x,y,z = regular_on_sphere_points(1, number-1)
     theta,phi=  cart2spherical_receptors(x,y,z) 
     direction_sphcoords= np.array([theta,phi]).T
-    direction_cartcoords= np.array([x,y,z]).T
 
-    return direction_sphcoords, direction_cartcoords
+    return direction_sphcoords
      
 
-def fibionaaci_directions(radius, number):
+def fibionaaci_directions(number):
     # found this function on stackoverflow https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
     # thought it might be a good way to distribute the directions on a sphere
     indices = np.arange(0, number, dtype=float) + 0.5
     phi = np.arccos(1 - 2*indices/number)
     theta = np.pi * (1 + 5**0.5) * indices
-    x = radius * np.cos(theta) * np.sin(phi)
-    y = radius * np.sin(theta) * np.sin(phi)
-    z = radius * np.cos(phi)
+    x = np.cos(theta) * np.sin(phi)
+    y = np.sin(theta) * np.sin(phi)
+    z = np.cos(phi)
     direction_sphcoords= np.array([theta,phi]).T
-    direction_cartcoords= np.array([x,y,z]).T
     
-    return direction_sphcoords, direction_cartcoords
-     
-    
-def eq_spaced_directions(radius, number):
- # I don't know how to run this matlab function sorry
-    eng = matlab.engine.start_matlab()
-    eng.eq_point_set_polar(2,3)
+    return direction_sphcoords
     
 
 
@@ -60,8 +49,10 @@ def ideal_direction(source_theta, source_phi, direction_sphcoords, radius):
 
     idx = np.where(distance == np.amin(distance))
     best_direction = direction_sphcoords[idx[0],:]
+    Y = np.zeros(len(direction_sphcoords))
+    Y[idx[0]] = 1
   
-    return best_direction
+    return Y
     
 
 if __name__ == '__main__':
