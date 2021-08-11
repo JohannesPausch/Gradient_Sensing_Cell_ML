ab086bdc4ccbb72170690bc939dc43da7602a6f7
Johannes Pausch <JohannesPausch@users.noreply.github.com>
2021-08-11 17:11:10 +0100

    fixed fifo and interface

--- BrownianParticle_fifo.c
+++ BrownianParticle_fifo.c
@@ -184,7 +184,7 @@ double source_distance2, sphere_distance2;
 
 
 setlinebuf(stdout);
-while ((ch = getopt(argc, argv, "c:d:i:N:o:r:s:S:t:w:")) != -1) {
+while ((ch = getopt(argc, argv, "c:d:i:N:o:R:r:s:S:t:w:")) != -1) {
   switch (ch) {
     case 'c':
       param_cutoff=strtod(optarg, NULL);
@@ -201,11 +201,12 @@ while ((ch = getopt(argc, argv, "c:d:i:N:o:r:s:S:t:w:")) != -1) {
     case 'o':
       STRCPY(param_output, optarg);
       break;
-    case 'ra':
+    case 'R':
       param_release_rate=strtod(optarg, NULL);
       break;
     case 'r':
       param_sphere_radius=strtod(optarg,NULL);
+      break;
     case 's':
       {
       char buffer[2048];
