972f369489f46b704855fd74600e56b67838aab0
paulagrcg <paulagrcg@gmail.com>
2021-08-18 17:15:21 +0100

    update

--- haversine.py
+++ haversine.py
@@ -4,7 +4,7 @@ import math
 #Calculates the min distance on surface of sphere between two points of different theta,phi coords.
 def haversine(R,theta1,phi1,theta2,phi2):
     dphi      = phi2 - phi1 #longitude conversion not necessary fifo.py does -180 to 180
-    dtheta    = (math.pi/2-theta2) - (math.pi/2-theta1) #latitude conversion since should be -90 to 90 but theta is 0 to 180
+    dtheta    = (math.pi/2-theta2) - (math.pi/2-theta1) #latitude conversion since should be -90 to 90 but theta is 0 to 180, nothing changes
     a = np.power(np.sin(dtheta/2),2) + np.cos(theta1)*np.cos(theta2)*np.power(np.sin(dphi/2),2)
     return 2*R*np.arctan2(np.sqrt(a), np.sqrt(1 - a))
 
--- sphericaltransf.py
+++ sphericaltransf.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 def cart2spherical_point(x,y,z):
-    r = math.pow(x,2) + math.pow(y,2) + math.pow(z,2)
+    r = math.pow(x,2) + math.pow(y,2) + math.pow(z,2) #not useful for this problem
     if (z!=0.):
         theta=math.atan(np.sqrt(x*x + y*y)/z)
         if (z<0.): theta+=math.pi
@@ -12,11 +12,11 @@ def cart2spherical_point(x,y,z):
 
 def cart2spherical_array(x,y,z):
     length = len(x)
-    r = np.zeros((length))
+    r = np.zeros((length)) #not useful for this problem
     theta = np.zeros((length))
     phi = np.zeros((length))
     for i in range(0,length):
-        r[i], theta[i], phi[i] = cart2spherical_point(x[i],y[i],z[i]) 
+        theta[i], phi[i] = cart2spherical_point(x[i],y[i],z[i]) 
     return theta,phi
 
 def spherical2cart_point(theta,phi):
