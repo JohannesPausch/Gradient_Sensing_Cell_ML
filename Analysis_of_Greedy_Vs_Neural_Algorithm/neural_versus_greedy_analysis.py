c2d8942bd2c0617210b5f0d83c5657e5061f2a98
Talia Rahall <74318567+tr4018@users.noreply.github.com>
2021-11-07 17:47:08 +0000

    Update neural_versus_greedy_analysis.py
    just added read data file in here to avoid path confusion

--- Analysis_of_Greedy_Vs_Neural_Algorithm/neural_versus_greedy_analysis.py
+++ Analysis_of_Greedy_Vs_Neural_Algorithm/neural_versus_greedy_analysis.py
@@ -4,38 +4,44 @@ import scipy.stats
 import os
 import sys
 
+def read_datafile(filename):
+    with open(filename+'.txt','r') as fin:
+        a = []
+        for line in fin:
+            if line.startswith('#'): pass
+            else:
+                b = []
+                line = line.split()
+                for x in line: 
+                    b.append(float(x))
+                a.append(b)
+    return(a)
 
+neuraldatadiff1 = read_datafile('neural_network_steps_datacutoff20diffusion1')
+greedydatadiff1 = read_datafile('greedy_algorithm_steps_datacutoff20diffusion1')
 
+neuraldatadiff2 = read_datafile('neural_network_steps_datacutoff20diffusion2')
+greedydatadiff2 = read_datafile('greedy_algorithm_steps_datacutoff20diffusion2')
 
-sys.path.append('/Users/Paula/Documents/GitHub/Gradient_Sensing_Cell_ML/Code')
-#os.path.join(os.path.dirname(sys.path[0]),'Code'
-from datawriteread import read_datafile
-neuraldata = read_datafile('neural_network_steps_data')
-greedydata = read_datafile('greedy_algorithm_steps_data')
+dist = np.arange(2,14,1)
 
-dist = np.arange(2,15,1)
+data = [neuraldatadiff1, greedydatadiff1, neuraldatadiff2, greedydatadiff2]
+colors = ['black', 'blue', 'green', 'red']
+labels = ['NN diff=1', 'Greedy diff=1', 'NN diff=2', 'Greedy diff=2']
+i=0
+for data_set in data:
 
-mean_neural =[]
-std_neural =[]
-
-for i in neuraldata:
-    mean_neural.append(np.mean(i))
-    std_neural.append(np.std(i))
-
-mean_greedy =[]
-std_greedy =[]
-
-for j in greedydata:
-    mean_greedy.append(np.mean(j))
-    std_greedy.append(np.std(j))
-
-
-plt.plot(dist, mean_neural, color='brown', label='Neural Network')
-plt.plot(dist, mean_greedy, color='green', label='Greedy Algorithm')
-plt.errorbar(dist, mean_neural, yerr=std_neural, color='brown',fmt='o',  markersize=6, capsize=5)
-plt.errorbar(dist, mean_greedy, yerr=std_greedy, color='green',fmt='o',  markersize=6, capsize=5)
+    mean_data = []
+    std_data = []
+    for j in range(0,len(dist)):
+        mean_data.append(np.mean(data_set[j]))
+        std_data.append(np.std(data_set[j]))
+    plt.plot(dist, mean_data, color=colors[i])
+    plt.errorbar(dist, mean_data, yerr=std_data, color=colors[i],fmt='o',  markersize=6, capsize=5, label=labels[i])
+    i+=1
 plt.legend()
 plt.xlabel('Distance of Cell from Source')
 plt.ylabel('Steps to find source')
 plt.grid()
 plt.show()
+        
